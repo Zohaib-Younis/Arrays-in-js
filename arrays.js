e6e42bcb3abbad80224998a2edfb67ac133fe129
Zohaib-Younis <mr.zohaibyounus@gmail.com>
2024-02-28 22:36:34 +0500

    arrys completed

--- arrays.js
+++ arrays.js
@@ -40,10 +40,77 @@ console.log(`So the average result is =`,average);
                //Practice question 2
 
 let price=[250,645,300,900,50];
-
+console.log(`Original values of items = ${price}`)
 for(let i=0;i<price.length;i++){
     let offer = price[i]/10;
     price[i]=price[i]-offer;
 }
-console.log(`Value after offer ${price}`);
+console.log(`Value after offer 10 persent off = ${price} `);
+
+             
+                // Array Methods
+
+   // push(); method add to end of array
+ let foodItems = ["potato","tomato","ginger","aplle"];
+ console.log(foodItems);
+
+ // will be add at end 
+ foodItems.push("Chips","paneer","burgur");
+ console.log(foodItems);
+
+
+
+                // Delete from end and return
+    
+let vages=["tomato","potato","ginger","onion"];
+
+vages.pop();
+console.log(vages);  // onion will be deleted from end of the array
+
+
+
+
+let deletedItem=vages.pop();
+console.log(`Deleted:`,deletedItem);
+           
+// toString();
+console.log(vages.toString()); 
+
+
+let markss=[35,567,78,54];
+marks.toString();
+console.log(marks);
+
+
+
+//concate 
+
+let heros=["thor","spiderman","ironman"];
+
+let dc_heros=["Supermna","Salman"];
+
+let hero=heros.concat(dc_heros);
+
+heros.unshift("akshay"); 
+console.log(hero);
+
+// slice method not change in orignal array
+
+let antman=["ant man","ali","rakha"];
+console.log(antman);
+console.log(antman.slice(1,3));
+
+
+//splice method is used to change in orignal array add delete and re-place
+
+let arr=[1,2,3,4,5,6,7];
+arr.splice(2,2,101,102);
+console.log(arr);
+
+let compnies=["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+console.log(compnies.splice(2, 1, "Ola"));
+
+compnies.push("Amazon");
+
+
 
